bd8f7142415e043b2a5bb07a6c2dc6760fd2a265
MieSaF <19381137+miesaf@users.noreply.github.com>
2020-02-06 16:42:27 +0800

    Update connectDB.php

--- connectDB.php
+++ connectDB.php
@@ -1,14 +1,14 @@
 <?php
-	$db_host	= "mysql.hostinger.my";
-	$db_uname	= "u681964154_pncrd";
-	$db_pword	= "O28GE>W#]:xARp";
-	$db_dbname	= "u681964154_pncrd";
+	$db_host	= "";
+	$db_uname	= "";
+	$db_pword	= "";
+	$db_dbname	= "";
 	
 	//connect to MySQL database server
 	$connection = mysql_connect($db_host, $db_uname, $db_pword) 
 				or die("Failed MySQL server connection attempt.<br>" . mysql_error() . "<br>");
 
 	//connecting to database
-	$selection = mysql_select_db("u681964154_pncrd") 
+	$selection = mysql_select_db($db_dbname) 
 		or die("Fail to connect to database.<br>" . mysql_error() . "<br>");
 ?>
